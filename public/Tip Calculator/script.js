a758e7771a6c2cd71474018420760e6d29bea03c
KingRavo18 <rkingovskis14@gmail.com>
2025-08-31 13:56:07 +0300

    s

---
+++ public/Tip Calculator/script.js
@@ -0,0 +1,20 @@
+function calculateTotal(){
+    const cashAmount = document.getElementById("cashAmount"),
+          tipPercentage = document.getElementById("tipPercentage"),
+          result = document.getElementById("total");
+
+        if(Number(cashAmount.value) === 0 || cashAmount.value < 0 || tipPercentage.value < 0){
+            result.style.color = "red";
+            result.style.fontSize = "100%"
+            if(Number(cashAmount.value) === 0){
+                result.textContent = "Please enter a cash amount";
+            }else{
+                result.textContent = "Both inputs must contain positive values";
+            }
+            return;
+        }
+
+    result.style.color = "black";
+    result.style.fontSize = "150%"
+    result.textContent = (Number(cashAmount.value) + (cashAmount.value / 100 * tipPercentage.value)).toFixed(2);
+}
