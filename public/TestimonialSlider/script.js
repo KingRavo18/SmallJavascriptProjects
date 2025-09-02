f198e9e8a3b7d7207b393d2b9663bda4a5dfddac
KingRavo18 <rkingovskis14@gmail.com>
2025-09-02 20:56:44 +0300

    r

--- public/TestimonialSlider/script.js
+++ public/TestimonialSlider/script.js
@@ -2,19 +2,23 @@ document.addEventListener("DOMContentLoaded", initializeSlider);
 
 function initializeSlider() {
     const slides = document.querySelectorAll(".testimonial-container");
+    const slideOutAnim = () => slides[slideIndex].classList.add("slideOutAnimation");
     let slideIndex = 0;
 
     if(slides.length > 0){
         slides[slideIndex].style.display = "block";
+        setTimeout(slideOutAnim, 6000);
         setInterval(() => {
             slideIndex++;
             slides.forEach(slide => {
                 slide.style.display = "none";
+                slide.classList.remove("slideOutAnimation");
             });
             if(slideIndex >= slides.length){
                 slideIndex = 0;
             }
             slides[slideIndex].style.display = "block";
-        }, 5000);
+            setTimeout(slideOutAnim, 6000);
+        }, 8000);
     }
 }
