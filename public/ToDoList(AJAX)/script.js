de146811a3606a845089c607a43736b9ad7b9824
KingRavo18 <rkingovskis14@gmail.com>
2025-09-13 16:24:54 +0300

    c

--- public/ToDoList(AJAX)/script.js
+++ public/ToDoList(AJAX)/script.js
@@ -1,29 +1,42 @@
 document.addEventListener("DOMContentLoaded", retrieveData);
 
-function submitTask(){
+function submitTask(event){
+    event.preventDefault();
+    const task = document.getElementById("taskInput").value;
+    const params = `task=${task}`;
 
+    const xhr = new XMLHttpRequest();
 }
 
-function retrieveData(){
+function retrieveData(event){
+    event.preventDefault();
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "./retrieveData.php", true);
 
     xhr.onload = function(){
         if(this.status === 200){
             const tasks = JSON.parse(this.responseText);
-            let output = "";
-
             tasks.forEach(task => {
-                output += `<li>
-                    ${task.task}
-                    <div>
-                        <button>✅</button>
-                        <button>🗑️</button>
-                    </div>
-                </li>`;
+                createListItem(task.task);
             });
-            document.getElementById("taskContianer").innerHTML = output;
         }
     }
     xhr.send();
+}
+
+function createListItem(taskContents){
+    const listItem = document.createElement("li");
+    const buttonContainer = document.createElement("div");
+    const finishedBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+
+    listItem.textContent = taskContents;
+    finishedBtn.textContent = "✅";
+    deleteBtn.textContent = "🗑️";
+
+    buttonContainer.appendChild(finishedBtn);
+    buttonContainer.appendChild(deleteBtn);
+    listItem.appendChild(buttonContainer);
+
+    document.getElementById("taskContianer").append(listItem);
 }
