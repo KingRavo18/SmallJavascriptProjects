05ab8cc7f91e1e613da09aa857ea501b370da562
KingRavo18 <rkingovskis14@gmail.com>
2025-09-22 13:25:59 +0300

    d

--- public/Temperature Converter/script.js
+++ public/Temperature Converter/script.js
@@ -1,21 +1,32 @@
-const celsiusInput = document.getElementById("celsiusInput"),
-      fahrenheitInput = document.getElementById("fahrenheitInput"),
-      kelvinInput = document.getElementById("kelvinInput"),
+document.addEventListener("DOMContentLoaded", () => {
+    const tempConvert = new TemperatureConverter();
+    document.getElementById("celsiusInput").addEventListener("input", () => tempConvert.fromCelsius());  
+    document.getElementById("fahrenheitInput").addEventListener("input", () => tempConvert.fromFahrenheit());  
+    document.getElementById("kelvinInput").addEventListener("input", () => tempConvert.fromKelvin());  
+});
 
-fromCelsius = () => {
-    const celsius = Number(celsiusInput.value);
-    fahrenheitInput.value = (celsius * 9 / 5 + 32).toFixed(2);
-    kelvinInput.value = (celsius + 273.15).toFixed(2);
-},
+class TemperatureConverter{
+    constructor(){
+        this.celsiusInput = document.getElementById("celsiusInput");
+        this.fahrenheitInput = document.getElementById("fahrenheitInput");
+        this.kelvinInput = document.getElementById("kelvinInput");
+    }
 
-fromFahrenheit = () => {
-    const fahrenheit = Number(fahrenheitInput.value);
-    celsiusInput.value = ((fahrenheit - 32) * 5 / 9).toFixed(2);
-    kelvinInput.value = ((fahrenheit - 32) * 5 / 9 + 273.15).toFixed(2);
-},
+    fromCelsius = () => {
+        const celsius = Number(this.celsiusInput.value);
+        this.fahrenheitInput.value = (celsius * 9 / 5 + 32).toFixed(2);
+        this.kelvinInput.value = (celsius + 273.15).toFixed(2);
+    }
 
-fromKelvin = () => {
-    const kelvin = Number(kelvinInput.value);
-    celsiusInput.value = (kelvin - 273.15).toFixed(2);
-    fahrenheitInput.value = ((kelvin - 273.15) * 9 /5 + 32).toFixed(2);
-};
+    fromFahrenheit = () => {
+        const fahrenheit = Number(this.fahrenheitInput.value);
+        this.celsiusInput.value = ((fahrenheit - 32) * 5 / 9).toFixed(2);
+        this.kelvinInput.value = ((fahrenheit - 32) * 5 / 9 + 273.15).toFixed(2);
+    } 
+
+    fromKelvin = () => {
+        const kelvin = Number(this.kelvinInput.value);
+        this.celsiusInput.value = (kelvin - 273.15).toFixed(2);
+        this.fahrenheitInput.value = ((kelvin - 273.15) * 9 /5 + 32).toFixed(2);
+    }
+}
