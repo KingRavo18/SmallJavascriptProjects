fedcde236474df99b86db19777e15a07ba51e70a
KingRavo18 <rkingovskis14@gmail.com>
2025-09-23 01:54:58 +0300

    f

--- public/TestimonialSlider/script.js
+++ public/TestimonialSlider/script.js
@@ -2,12 +2,9 @@
     document.addEventListener("DOMContentLoaded", createSlides);
 
     async function createSlides(){
-
         try{
             const response = await fetch("slideData.json");
-            if(!response.ok){
-                throw new Error("Could not fetch resource");
-            }
+            if(!response.ok) throw new Error("Could not fetch resource");
             const data = await response.json();
 
             data.forEach(dataObject => {
@@ -25,10 +22,10 @@
                 const signature = document.createElement("p");
                 signature.textContent = dataObject.signature;
 
-                document.getElementById("testimonial-slider-container").appendChild(testimonialContainer);
                 testimonialContainer.appendChild(image);
                 testimonialContainer.appendChild(testimonial);
                 testimonialContainer.appendChild(signature);
+                document.getElementById("testimonial-slider-container").appendChild(testimonialContainer);
             });
 
             initializeSlider();
@@ -44,21 +41,21 @@
         let timeout = null;
         const slideOutAnim = () => slides[slideIndex].classList.add("slideOutAnimation");
 
-        if(slides.length > 0){
+        if(slides.length <= 0) return;
+
+        slides[slideIndex].classList.add("visible");
+        timeout = setTimeout(slideOutAnim, 6000);
+        setInterval(() => {
+            clearTimeout(timeout);
+            slides[slideIndex].classList.remove("visible");
+            slides[slideIndex].classList.remove("slideOutAnimation");
+            slideIndex++;
+            if(slideIndex >= slides.length){
+                slideIndex = 0;
+            }
             slides[slideIndex].classList.add("visible");
-            setTimeout(slideOutAnim, 6000);
-            setInterval(() => {
-                clearTimeout(timeout);
-                slides[slideIndex].classList.remove("visible");
-                slides[slideIndex].classList.remove("slideOutAnimation");
-                slideIndex++;
-                if(slideIndex >= slides.length){
-                    slideIndex = 0;
-                }
-                slides[slideIndex].classList.add("visible");
-                localStorage.setItem("slideIndex", slideIndex);
-                timeout = setTimeout(slideOutAnim, 6000);
-            }, 8000);
-        }
+            localStorage.setItem("slideIndex", slideIndex);
+            timeout = setTimeout(slideOutAnim, 6000);
+        }, 8000);
     }
 })();
