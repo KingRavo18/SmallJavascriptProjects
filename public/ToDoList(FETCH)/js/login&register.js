18f9fdd847ceb14101ecab8babaed903e7746edc
KingRavo18 <rkingovskis14@gmail.com>
2025-10-01 18:45:38 +0300

    g

---
+++ public/ToDoList(FETCH)/js/login&register.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("registrationForm").addEventListener("submit", (event) => registration(event));
+});
+
+async function registration(event) {
+    event.preventDefault();
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const responseMessage = document.getElementById("registration-message");
+    responseMessage.style.color = "green";
+    responseMessage.textContent = "";
+
+    try {
+        inputValidation(username, password)
+
+        const response = await fetch("./php/register.php", {
+            method: "POST",
+            headers: { "Content-type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({ username: username.value, password: password.value })
+        });
+        if (!response.ok) {
+            throw new Error("Could not register");
+        }
+
+        const data = await response.json();
+
+        username.value = "";
+        password.value = "";
+    } catch (error) {
+        responseMessage.style.color = "red";
+        responseMessage.textContent = error.message;
+    }
+}
+
+function inputValidation(username, password) {
+    if (username.value.trim() == "") {
+        throw new Error("Please input a username");
+    }
+    if (password.value.trim() == "") {
+        throw new Error("Please input a password");
+    }
+    if (password.value.length < 8) {
+        throw new Error("A password must be at least 8 symbols long");
+    }
+}
--- public/ToDoList(FETCH)/php/register.php
+++ public/ToDoList(FETCH)/php/register.php
@@ -1,31 +1,29 @@
 <?php
 require("database.php");
 
-if (isset($_POST["registerBTN"])) {
-    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if (empty(trim($username))) {
-        echo json_encode(["query_fail" => "Please enter an username"]);
-        exit;
-    }
-    if (empty(trim($password))) {
-        echo json_encode(["query_fail" => "Please enter a password"]);
-        exit;
-    }
+if (empty(trim($username))) {
+    echo json_encode(["query_fail" => "Please enter an username"]);
+    exit;
+}
+if (empty(trim($password))) {
+    echo json_encode(["query_fail" => "Please enter a password"]);
+    exit;
+}
 
-    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    try {
-        $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-        $stmt->execute([$username, $password_hash]);
-        echo json_encode(["query_success" => "You are now registered"]);
-    } catch (PDOException $e) {
-        echo json_encode(["query_fail" => "Caught exception: {$e->getMessage()}"]);
-    } finally {
-        session_destroy();
-        header("Location: ../index.html");
-    }
+try {
+    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->execute([$username, $password_hash]);
+    echo json_encode(["query_success" => "You are now registered"]);
+} catch (PDOException $e) {
+    echo json_encode(["query_fail" => "Caught exception: {$e->getMessage()}"]);
+} finally {
+    session_destroy();
+    header("Location: ../index.html");
 }
 
 $stmt = null;
